0d1e22e51fe54e1af44da335dd6abe3643811166
ThE <42667961+EnsnerT@users.noreply.github.com>
2019-02-20 09:40:08 +0100

    Update haf.js

--- haf.js
+++ haf.js
@@ -1,3 +1,3 @@
 // script fur Header und Footer
 document.body.onload=function(){document.body.getElementsByClassName("header")[0].innerHTML=
-'<a href="index.html" alt="Home"><img src="logo.svg" width=40> </a><a href="adp.html">Arten der Parfüme</a><a href="marken.html">Marken</a><a href="auftragen.html">Auftrage Stellen</a>';}
+'<a href="index.html" alt="Home"><img src="logo.svg" width=40> </a><a href="adp.html">Arten der Parfüme</a><a href="marken.html">Marken</a><a href="auftragen.html">Auftrage Stellen</a><a href="haltbarkeit.html">Haltbarkeit</a>';}
