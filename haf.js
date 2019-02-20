1943ea5b3745b41ab622563ba0493cb555be2fde
ThE <42667961+EnsnerT@users.noreply.github.com>
2019-02-20 10:05:39 +0100

    Update haf.js

--- haf.js
+++ haf.js
@@ -1,3 +1,16 @@
 // script fur Header und Footer
-document.body.onload=function(){document.body.getElementsByClassName("header")[0].innerHTML=
-'<a href="index.html" alt="Home"><img src="logo.svg" width=40> </a><a href="adp.html">Arten der Parfüme</a><a href="marken.html">Marken</a><a href="auftragen.html">Auftrage Stellen</a><a href="haltbarkeit.html">Haltbarkeit</a><a href="top.html">Die besten Düfte</a><a href="aboutus.html">Über uns</a>';}
+document.body.onload=function(){
+  var header='';
+  header+='<a href="index.html" alt="Home"><img src="logo.svg" width=40></a>';
+  header+='<a href="adp.html">Arten der Parfüme</a>';
+  header+='<a href="marken.html">Marken</a>';
+  header+='<a href="auftragen.html">Auftrage Stellen</a>';
+  header+='<a href="haltbarkeit.html">Haltbarkeit</a>';
+  header+='<a href="top.html">Die besten Düfte</a>';
+  header+='<a href="aboutus.html">Über uns</a>';
+  document.body.getElementsByClassName("header")[0].innerHTML=header;
+  
+  var footer='';
+  footer+='<br>';
+  document.body.getElementsByClassName("footer")[0].innerHTML=footer;
+}
